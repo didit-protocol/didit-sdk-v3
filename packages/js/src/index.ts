fbc24d95910ceae564b2b4e75682cc8e53434218
keddib <khalid.eddib@gamiumworld.com>
2024-07-03 16:29:36 +0200

    fix(package.json lock.json): fix deps versions

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -3,13 +3,11 @@ import { DiditSdk, ConstantsUtil } from '@didit-sdk/core'
 
 export type { DiditSdk, DiditAuthStatus, DiditAuthMethod } from '@didit-sdk/core'
 
-type DiditSdkCoreOptions = DiditClientOptions<CoreConfig>
-
 export type DiditSdkOptions = Omit<DiditClientOptions<CoreConfig>, '_sdkVersion'>
 
 export { defaultWagmiCoreConfig as defaultWagmiConfig } from '@didit-sdk/core'
 
-export function createDiditSdk(options: DiditSdkCoreOptions) {
+export function createDiditSdk(options: DiditSdkOptions) {
   return new DiditSdk({
     ...options,
     _sdkVersion: `didit-sdk-js-${ConstantsUtil.DIDIT_SKD_VERSION}`
