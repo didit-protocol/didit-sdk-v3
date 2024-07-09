c5653e98fe9ec72ea2e8205adf1976dca3d8c573
keddib <khalid.eddib@gamiumworld.com>
2024-07-09 16:31:49 +0200

    build(packages/ui/tests): fix ci test command

--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
+
 module.exports = {
   root: true,
-  extends: ['plugin:require-extensions/recommended', '@didit-sdk/eslint-config/base.js'],
+  extends: ['@didit-sdk/eslint-config/base.js', 'plugin:require-extensions/recommended'],
   plugins: ['require-extensions']
 }
